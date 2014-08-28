74c5a4ae65f76861724e2c0ca8c7c161db708028
maria-negrea <m.maria_negrea@yahoo.com>
2014-08-28 10:34:46 +0300

    added star

--- TestFlag/Example1.cpp
+++ TestFlag/Example1.cpp
@@ -46,7 +46,7 @@ inline double rgb(int old);
 void display2()                    
 {
     glClear (GL_COLOR_BUFFER_BIT);
-    glColor3f (rgb(255), rgb(0), rgb(0));
+    glColor3f (rgb(223), rgb(13), rgb(14));
 	glTranslatef(0.0, 0.0 , -6.8);
     glutSolidSphere(0.05,500, 500); 
     glFlush();
@@ -73,7 +73,7 @@ void display2()
 	glBegin(GL_LINE_STRIP);
 		count = 0;
 		for(double i = -1; i < 1; i+=0.1){
-			Point2D temp(i-0.75, cos(PI*i)/3+0.32);
+			Point2D temp(i-1, cos(PI*i)/3+0.32);
 			glVertex3f(temp.x, temp.y, 0.0);
 			count++;
 			vec2.push_back(temp);
@@ -90,20 +90,20 @@ void display2()
 			
 		}
 	glEnd();
-	glRotatef(-10, 0.0, 0.0, -6.8);
-	glColor3f (rgb(255), rgb(0), rgb(0));
+	glRotatef(0, 0.0, 0.0, -6.8);
+	glColor3f (rgb(223), rgb(13), rgb(14));
     glBegin(GL_LINE_STRIP);
 		for(double i =-0.6; i < 0.6;i+=0.1){
 			glLineWidth(10.0);
-			Point2D temp(i-0.48, 0.65*i*i+0.1*i-0.2);
+			Point2D temp(i-0.33, 0.65*i*i+0.1*i-0.12);
 			glVertex3f(temp.x, temp.y, 0.0);
 		}
     glEnd();
 
-	glTranslatef(1.2,0.0,2.8);
-	glRotatef(20, 1.2, 0.0, 2.8);
-	glColor3f(rgb(255), rgb(255), rgb(255));
-	DrawStar(0.0, 0.0, 0.5, 0.2);
+	glTranslatef(-0.6,0.05,2.8);
+	/*glRotatef(20, 1.2, 0.0, 2.8);*/
+	glColor3f(rgb(223), rgb(13), rgb(14));
+	DrawStar(0.0, 0.0, 0.1, 0.05);
 	glFlush();
 
     glFlush ();      //forces previously issued commands to execute
